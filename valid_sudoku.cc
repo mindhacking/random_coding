b776cc4ce2977b5b3a608d57d6a433de09452eee
mindhacking <brian.zy.liu@gmail.com>
2015-01-04 19:48:27 -0600

    Modify sudoku code

--- valid_sudoku.cc
+++ valid_sudoku.cc
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
     
-        int rowValid[10] = {0};//用于判断某一行是否合法，对于行来说这个数组可以重复使用
-        int columnValid[9][10] = {0};//用于判断某一列是否合法
-        int subBoardValid[9][10] = {0};//用于判断某一个九宫格是否合法
+        int rowValid[10] = {0};
+        int columnValid[9][10] = {0};
+        int subBoardValid[9][10] = {0};
         for(int i = 0; i < 9; i++)
         {
             memset(rowValid, 0, sizeof(rowValid));
